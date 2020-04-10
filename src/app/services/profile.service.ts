45c4b81713c98f2bc9724fbb6aed6b98ffb62358
luqqy <luckyoula@gmail.com>
2020-04-10 16:37:41 +0300

    modified services

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient }from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+  private username:string
+  private clientid:""
+  private clientsecret:""
+  constructor(private http:HttpClient ) { 
+    console.log("service is now ready")
+    this.username ="luckyoula";
+  }
 
-  constructor() { }
+getprofileInfo(){
+ return this.http.get ()
+}
+
 }
