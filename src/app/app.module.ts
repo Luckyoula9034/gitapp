87852d2b63b6112c9c73702e12935e42863e7336
luqqy <luckyoula@gmail.com>
2020-04-10 17:28:38 +0300

    updated modules.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { ProfileService } from './services/profile.service';
+import { HttpClient }from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+     
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+     
   ],
   providers: [ProfileService ],
   bootstrap: [AppComponent]
